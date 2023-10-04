ddc8ad478e754fd62dfcc147af40c535a4be956e
Jeroen Rinzema <jeroen@rinzema.dev>
2023-10-04 11:38:55 +0200

    feat: include seo metadata

--- pages/index.vue
+++ pages/index.vue
@@ -8,3 +8,31 @@
         <Footer />
     </div>
 </template>
+
+<script setup>
+useSeoMeta({
+    title: 'Cloud Proud, a single access point for all your data!',
+    description: 'Simply connect your data sources, no copies or ETL pipelines required. Query your data in real-time.',
+    ogTitle: 'Cloud Proud, a single access point for all your data!',
+    ogDescription: 'Simply connect your data sources, no copies or ETL pipelines required. Query your data in real-time.',
+    ogImage: '/og-image.jpg',
+    ogUrl: 'https://cloudproud.nl',
+    twitterTitle: 'Cloud Proud, a single access point for all your data!',
+    twitterDescription: 'Simply connect your data sources, no copies or ETL pipelines required. Query your data in real-time.',
+    twitterImage: '/twitter-image.jpg',
+    twitterCard: 'summary'
+})
+
+useHead({
+    htmlAttrs: {
+        lang: 'en'
+    },
+    link: [
+        {
+            rel: 'icon',
+            type: 'image/png',
+            href: '/favicon.png'
+        }
+    ]
+})
+</script>
