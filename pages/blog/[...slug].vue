3890684a1d1e4242e7fe8e1c4fa08c71f625e17d
givenchy makela <makelagivenchy@gmail.com>
2023-10-04 16:54:23 +0200

    blog

---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="px-6 py-32 lg:px-8">
+            <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+                <ContentDoc>
+                    <template v-slot="{doc}">
+                        <Section id="main">
+                            <ContentRenderer :value="doc" class="blog-content" />
+                        </Section>
+                    </template>
+                </ContentDoc>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.blog-content h1 {
+    @apply mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl;
+}
+
+.blog-content h2 {
+    @apply mt-16 text-2xl font-bold tracking-tight text-gray-900;
+}
+
+.blog-content p {
+    @apply mt-6;
+}
+
+.blog-content img {
+    @apply aspect-video rounded-xl bg-gray-50 object-cover;
+}
+</style>
